ab196e4e0fc97dfef2c94204449f34969ee6127e
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-02-19 17:11:59 +0000

    Replace custom test util functions with avoriaz

--- test/unit/components/Swatch.test.js
+++ test/unit/components/Swatch.test.js
@@ -1,36 +1,26 @@
-import Vue from 'vue';
 import { expect } from 'chai';
+import { mount } from 'avoriaz';
 import 'babel-polyfill';
 import Swatch from '../../../src/components/Swatch';
 
-function getRenderedText(Component, propsData, el) {
-  const Ctor = Vue.extend(Component);
-  const vm = new Ctor({ propsData }).$mount();
-  return vm.$el.querySelector(el).textContent;
-}
-
-function getRenderedStyle(Component, propsData, el) {
-  const Ctor = Vue.extend(Component);
-  const vm = new Ctor({ propsData }).$mount();
-  if (el) {
-    return vm.$el.querySelector(el).style;
-  }
-  return vm.$el.style;
-}
 
 describe('Swatch', () => {
   it('renders propsData.color as text in .swatch-info', () => {
     const color = 'rgb(0, 0, 0)';
-    expect(getRenderedText(Swatch, { color }, '.swatch-info')).to.equal(color);
+    const wrapper = mount(Swatch, { propsData: { color, tone: 'tone' } });
+    const swatchInfo = wrapper.find('.swatch-info')[0];
+    expect(swatchInfo.text()).to.equal(color);
   });
 
   it('renders .swatch background style to propsData.color', () => {
     const color = 'rgb(0, 0, 0)';
-    expect(getRenderedStyle(Swatch, { color }).backgroundColor).to.equal(color);
+    const wrapper = mount(Swatch, { propsData: { color, tone: 'tone' } });
+    expect(wrapper.hasStyle('background-color', color)).to.equal(true);
   });
-
   it('renders .swatch-info color style to propsData.tone', () => {
     const tone = 'rgb(0, 0, 0)';
-    expect(getRenderedStyle(Swatch, { tone }, '.swatch-info').color).to.equal(tone);
+    const wrapper = mount(Swatch, { propsData: { color: 'red', tone } });
+    const swatchInfo = wrapper.find('.swatch-info')[0];
+    expect(swatchInfo.hasStyle('color', tone)).to.equal(true);
   });
 });
