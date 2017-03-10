effb32566327261b70f45c0c4b13a484d907c16e
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-03-10 20:28:21 +0000

    Refactor GeneratePalette test

--- test/unit/components/GeneratePalette.test.js
+++ test/unit/components/GeneratePalette.test.js
@@ -4,22 +4,24 @@ import { expect } from 'chai';
 import Vuex from 'vuex';
 import GeneratePalette from '../../../src/components/GeneratePalette';
 
+avoriaz.use(Vuex);
 
 describe('GeneratePalette.vue', () => {
-  it('calls store action generateRandomPalette when random-palette is clicked', () => {
-    avoriaz.use(Vuex);
-    const actions = {
-      generateRandomPalette: sinon.stub(),
+  let actions;
+  let store;
+
+  beforeEach(() => {
+    actions = {
       generatePalette: sinon.stub(),
+      generateRandomPalette: sinon.stub(),
     };
-    const store = new Vuex.Store({
-      modules: {
-        palette: {
-          state: {},
-          actions,
-        },
-      },
+    store = new Vuex.Store({
+      state: {},
+      actions,
     });
+  });
+
+  it('calls store action generateRandomPalette when random-palette is clicked', () => {
     const wrapper = mount(GeneratePalette, { store });
     const input = wrapper.find('#generate-palette')[0];
     input.element.value = '#666';
@@ -28,19 +30,6 @@ describe('GeneratePalette.vue', () => {
   });
 
   it('calls store action generateRandomPalette when random-palette is clicked', () => {
-    avoriaz.use(Vuex);
-    const actions = {
-      generateRandomPalette: sinon.stub(),
-      generatePalette: sinon.stub(),
-    };
-    const store = new Vuex.Store({
-      modules: {
-        palette: {
-          state: {},
-          actions,
-        },
-      },
-    });
     const wrapper = mount(GeneratePalette, { store });
     wrapper.find('#random-palette')[0].simulate('click');
     expect(actions.generateRandomPalette.calledOnce).to.equal(true);
