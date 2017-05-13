472e395c13f8b8a41d6888c3165f148928bc10e4
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-05-13 23:17:41 +0100

    [New] Add HistoryList component

---
+++ src/components/HistoryList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="history-list">
+      <palette
+        v-for="palette in history"
+        :palette="palette"
+        className="palette--history-list"
+      />
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+  import Palette from '@/components/Palette';
+
+  export default{
+    name: 'history-list',
+    computed: mapGetters([
+      'history',
+    ]),
+    components: {
+      Palette,
+    },
+  };
+</script>
---
+++ test/unit/specs/components/HistoryList.spec.js
@@ -0,0 +1,31 @@
+import { mount } from 'avoriaz';
+import Vue from 'vue';
+import { expect } from 'chai';
+import Vuex from 'vuex';
+import HistoryList from '@/components/HistoryList';
+import Palette from '@/components/Palette';
+
+Vue.use(Vuex);
+
+describe('HistoryList.vue', () => {
+  let getters;
+  let store;
+  let history;
+
+  beforeEach(() => {
+    history = [{}, {}, {}];
+    getters = {
+      history: () => history,
+    };
+
+    store = new Vuex.Store({
+      getters,
+    });
+  });
+
+  it('renders a palette for each object in state.history, passing state.history[i] as palette', () => {
+    const wrapper = mount(HistoryList, { store });
+    expect(wrapper.find(Palette).length).to.equal(history.length);
+    expect(wrapper.find(Palette)[0].vm.palette).to.equal(history[0]);
+  });
+});
