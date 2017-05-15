d55c9ae843a6c54eb8b82fde6f23468ba9213748
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-05-15 12:58:43 +0100

    [Tests] Fix skipped tests

--- test/unit/specs/components/GeneratePaletteForm.spec.js
+++ test/unit/specs/components/GeneratePaletteForm.spec.js
@@ -13,7 +13,6 @@ describe('GeneratePaletteForm.vue', () => {
 
   beforeEach(() => {
     actions = {
-      generatePalette: sinon.stub(),
       generateRandomPalette: sinon.stub(),
     };
     store = new Vuex.Store({
@@ -21,14 +20,15 @@ describe('GeneratePaletteForm.vue', () => {
     });
   });
 
-  it.skip('calls store action generateRandomPalette when random-palette is clicked', () => {
-    const wrapper = mount(GeneratePaletteForm, { store });
+  it('calls store action generateRandomPalette when random-palette is clicked', () => {
     const rgb = 'rgb(0,0,0)';
-    wrapper.find('input')[0].element.value = rgb;
-    wrapper.find('input')[0].simulate('change');
+    const $store = { dispatch: sinon.stub() };
+    const wrapper = mount(GeneratePaletteForm, { globals: { $store } });
+    wrapper.find('#generate-palette')[0].element.value = rgb;
+    wrapper.find('#generate-palette')[0].simulate('input');
     wrapper.find('form')[0].simulate('submit');
-    expect(actions.generatePalette.calledOnce).to.equal(true);
-    expect(actions.generatePalette.calledWith({ rgb })).to.equal(true);
+    expect($store.dispatch.calledOnce).to.equal(true);
+    expect($store.dispatch.calledWith('generatePalette', { rgb })).to.equal(true);
   });
 
   it('calls store action generateRandomPalette when random-palette is clicked', () => {
