9a8a5c2aec73eacf6b26217404671e0998ee2501
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-05-18 21:48:32 +0100

    [Fix] Remove clashing names props in routes array

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,12 +3,10 @@ import PaletteGenerator from '@/components/PaletteGenerator';
 const routes = [
   {
     path: '/',
-    name: 'PaletteGenerator',
     component: PaletteGenerator,
   },
   {
     path: '/:hex',
-    name: 'PaletteGenerator',
     component: PaletteGenerator,
     props: true,
   },
